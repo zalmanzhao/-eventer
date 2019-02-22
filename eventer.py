3598497c8d6e66bfab2fce01c8a7f650d9210669
Zalman Zhao <840912357@qq.com>
2019-02-22 22:45:25 +0800

    Update eventer.py

--- eventer.py
+++ eventer.py
@@ -1,227 +1,272 @@
-from kubernetes import client, config, watch
+import kubernetes
 import datetime
 import json
 import requests
 import logging
 import sys
-import os
+import queue
+import signal
 import threading
+import argparse
 from time import mktime, time
 import hashlib
-from pytz import utc
-from pytz import timezone
-from elasticsearch import Elasticsearch
+import urllib3
+from elasticsearch import Elasticsearch, ConnectionTimeout
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+WATCH_TIMEOUT = 60
 POD_MSG_TEMPLATE = "### %s \n- Type:%s \n- Level:%s \n- Namespace:%s \n- Name:%s \n- Message:%s \n- Reason:%s \n- Timestamp:%s\n"
 NODE_MSG_TEMPLATE = "### %s \n- Type:%s \n- Level:%s \n- Name:%s \n- Message:%s \n- Reason:%s \n- Timestamp:%s\n"
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
 EVENT_DATA = {}
 
-def save_es(cluster_name, type, event, es_host):
-    cst_tz = timezone('Asia/Shanghai')
-    now = datetime.datetime.now().replace(tzinfo=cst_tz)
-    utctime = now.astimezone(utc)
-    try:
-        es = Elasticsearch(hosts=es_host)
-    except:
-        logging.error("初始化es失败")
-    else:
-        if type == 'event':
-            try:
-                doc = {}
-                doc['Message'] = event['object'].message
-                doc['Reason'] = event['object'].reason
-                doc['Name'] = event['object'].metadata.name
-                doc['Namespace'] = event['object'].metadata.namespace
-                doc['Type'] = 'event'
-                doc['Level'] = event['object'].type
-                doc['ClusterName'] = cluster_name
-                doc['Timestamp'] = (event['object'].last_timestamp + datetime.timedelta(hours=8)).strftime(
-                            '%Y-%m-%d %H:%M:%S')
-                doc['timestamp'] = utctime
-                es.index(index=cluster_name.lower(), doc_type="text", body=doc)
-            except:
-                logging.error("event保存es失败")
-        else:
-            try:
-                conditions = event['object'].status.conditions
-                for condition in conditions:
-                    if (condition.type == 'Ready' and condition.status != 'True') or (
-                            condition.type != 'Ready' and condition.status != 'False'):
-                        doc = {}
-                        doc['Message'] = condition.message
-                        doc['Reason'] = condition.reason
-                        doc['Name'] = event['object'].metadata.name
-                        doc['Type'] = 'node'
-                        doc['Level'] = 'Error'
-                        doc['ClusterName'] = cluster_name
-                        doc['Timestamp'] = (event['object'].last_timestamp + datetime.timedelta(hours=8)).strftime(
-                            '%Y-%m-%d %H:%M:%S')
-                        doc['timestamp'] = utctime
-                        es.index(index=cluster_name.lower(), doc_type="text", body=doc)
-            except:
-                logging.error("node事件保存es失败")
-                
 
-def get_md5(data):
-    m1 = hashlib.md5()
-    m1.update(data.encode("utf-8"))
-    token = m1.hexdigest()
-    return token
-
-
-#消息收敛
+# 消息收敛
 def event_convergence(content, times, interval, last_time):
-    md5_data = get_md5(content.replace(last_time, 'x'))
-    #第一次出现需要告警
+    m1 = hashlib.md5()
+    m1.update((content.replace(last_time, 'x')).encode("utf-8"))
+    md5_data = m1.hexdigest()
+    # 第一次出现需要告警
     if md5_data not in EVENT_DATA.keys():
         EVENT_DATA[md5_data] = {'times': 1, 'last_timestamp': int(time())}
         return True
     else:
-        #第二次出现需要告警
+        # 第二次出现需要告警
         if EVENT_DATA[md5_data]['times'] == 1:
             EVENT_DATA[md5_data] = {'times': 2, 'last_timestamp': int(time())}
             return True
         else:
-            #如果前面有第10次或者现在的时间隔上次发送的时间大于等于180秒则需要发送
+            # 如果前面有第10次或者现在的时间隔上次发送的时间大于等于180秒则需要发送
             if EVENT_DATA[md5_data]['times'] >= times or int(time()) - EVENT_DATA[md5_data]['last_timestamp'] >= interval:
                 EVENT_DATA[md5_data] = {'times': 1, 'last_timestamp': int(time())}
                 return True
             else:
                 EVENT_DATA[md5_data]['times'] = EVENT_DATA[md5_data]['times'] + 1
-                logging.info("事件次数小于%s次，或者距离上次告警时间相差不到%s秒，故而忽略本次事件告警，消息内容：%s" % (times, interval, content))
+                logging.error("事件次数小于%s次，或者距离上次告警时间相差不到%s秒，故而忽略本次事件告警，消息内容：%s" % (times, interval, content))
                 return False
 
 
-def send_ding(data, robot):
-    post_data = json.dumps(data).encode(encoding='UTF8')
-    response = requests.post(robot, headers={'Content-Type': 'application/json; charset=utf-8'}, data=post_data)
-    if response.status_code != 200:
-        return False
-    return True
+def print_handler(event):
+    if event['object'].kind == 'Event':
+        logging.error('STDOUT ' + (event['object'].last_timestamp + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' ' + event['object'].metadata.resource_version + ' ' + event['object'].metadata.name + ' ' + event['object'].message + ' ' + str(datetime.datetime.now()))
+    else:
+        logging.error('STDOUT ' +event['object'].metadata.resource_version + ' ' + event['object'].metadata.name + ' ' + str(datetime.datetime.now()))
 
 
-def pod_event(v1, level, cluster_name, robot, pod_at_all, times, interval, es_host):
-    logging.info("Pod事件监控子进程启动")
-    w = watch.Watch()
-    for event in w.stream(v1.list_event_for_all_namespaces):
-        if es_host != '':
-            save_es(cluster_name, 'event', event, es_host)
+class EventWatcherThread(threading.Thread):
+    def __init__(self, queue):
+        super().__init__(daemon=True)
+        self.queue = queue
+        self.resource_version = 0
+
+    def run(self):
         try:
-            if event['object'].type == level and int(time()) - int(
-                    mktime((event['object'].last_timestamp + datetime.timedelta(hours=8)).timetuple())) <= 30:
-                last_time = (event['object'].last_timestamp + datetime.timedelta(hours=8)).strftime(
-                    '%Y-%m-%d %H:%M:%S')
-                content = POD_MSG_TEMPLATE % (
-                '【' + cluster_name + '】 Pod事件监控', 'Pod', level, event['object'].metadata.namespace, event['object'].metadata.name,
-                event['object'].message, event['object'].reason, last_time)
-                if event_convergence(content, times, interval, last_time):
-                    data = {"msgtype": "markdown", "at": {"atMobiles": [], "isAtAll": pod_at_all}, "markdown": {"title": cluster_name, "text": content}}
-                    if not send_ding(data, robot):
-                        logging.error("Pod发送钉钉告警失败！")
-        except:
-            logging.error("解析event数据异常！")
+            return self._run()
+        except Exception as e:
+            e.thread = self
+            self.queue.put(e)
+            raise e
+
+    def _run(self):
+        while True:
+            start_time = datetime.datetime.now()
+            try:
+                self._watch(since_time=start_time)
+            except urllib3.exceptions.ReadTimeoutError:
+                logging.error('EventWatcher timeout')
+            else:
+                logging.error('EventWatcher connection closed')
+
+    def _watch(self, since_time):
+        logging.error('Event Watching events since %s', since_time)
+
+        w = kubernetes.watch.Watch()
+        configuration = kubernetes.client.Configuration()
+        configuration.verify_ssl = False
+        v1 = kubernetes.client.CoreV1Api(kubernetes.client.ApiClient(configuration))
+
+        for event in w.stream(v1.list_event_for_all_namespaces, resource_version=self.resource_version, _request_timeout=WATCH_TIMEOUT):
+            self.resource_version = event['object'].metadata.resource_version
+            self.queue.put(event)
 
 
-def node_event(v1, level, cluster_name, robot, node_at_all, es_host):
-    logging.info("Node异常监控子进程启动")
-    w = watch.Watch()
-    for event in w.stream(v1.list_node):
-        if es_host != '':
-            save_es(cluster_name, 'node', event, es_host)
+class NodeWatcherThread(threading.Thread):
+    def __init__(self, queue):
+        super().__init__(daemon=True)
+        self.queue = queue
+        self.resource_version = 0
+
+    def run(self):
         try:
+            return self._run()
+        except Exception as e:
+            e.thread = self
+            self.queue.put(e)
+            raise e
+
+    def _run(self):
+        while True:
+            start_time = datetime.datetime.now()
+            try:
+                self._watch(since_time=start_time)
+            except urllib3.exceptions.ReadTimeoutError:
+                logging.error('NodeWatcher timeout')
+            else:
+                logging.error('NodeWatcher connection closed')
+
+    def _watch(self, since_time):
+        logging.error('Node Watching events since %s', since_time)
+
+        w = kubernetes.watch.Watch()
+        configuration = kubernetes.client.Configuration()
+        configuration.verify_ssl = False
+        v1 = kubernetes.client.CoreV1Api(kubernetes.client.ApiClient(configuration))
+
+        for event in w.stream(v1.list_node, resource_version=self.resource_version, _request_timeout=WATCH_TIMEOUT):
+            self.resource_version = event['object'].metadata.resource_version
+            self.queue.put(event)
+
+
+class RobotHandler:
+    def __init__(self, cluster_name, level, ding_url, event_at_all, node_at_all):
+        self.cluster_name = cluster_name
+        self.level = level
+        self.ding_url = ding_url
+        self.event_at_all = event_at_all
+        self.node_at_all = node_at_all
+
+    def __call__(self, event):
+        if event['object'].kind == 'Event':
+            if self.level == event['object'].type and int(time()) - int(mktime((event['object'].last_timestamp + datetime.timedelta(hours=8)).timetuple())) <= 30:
+                last_time = (event['object'].last_timestamp + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+                content = POD_MSG_TEMPLATE % ('【' + self.cluster_name + '】 Event事件监控', 'Pod', self.level, event['object'].metadata.namespace, event['object'].metadata.name, event['object'].message, event['object'].reason, last_time)
+                if event_convergence(content, 10, 180, last_time):
+                    data = {"msgtype": "markdown", "at": {"atMobiles": [], "isAtAll": self.event_at_all}, "markdown": {"title": self.cluster_name, "text": content}}
+                    post_data = json.dumps(data).encode(encoding='UTF8')
+                    requests.post(self.ding_url, headers={'Content-Type': 'application/json; charset=utf-8'}, data=post_data)
+            else:
+                logging.error('丢弃 ' + (event['object'].last_timestamp + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S') + ' ' + event['object'].type + event['object'].metadata.resource_version + ' ' + event['object'].metadata.name + ' ' + event['object'].message + ' ' + str(datetime.datetime.now()))
+        else:
             conditions = event['object'].status.conditions
             for condition in conditions:
-                last_time = (condition.last_heartbeat_time + datetime.timedelta(hours=8)).strftime(
-                    '%Y-%m-%d %H:%M:%S')
-                if (condition.type == 'Ready' and condition.status != 'True') or (
-                        condition.type != 'Ready' and condition.status != 'False'):
-                    content = NODE_MSG_TEMPLATE % (
-                    '【' + cluster_name + '】 Node异常监控', 'Node', level, event['object'].metadata.name, condition.message, condition.reason,
-                    last_time)
-                    #Node异常告警不做消息收敛，需要及时处理
-                    data = {"msgtype": "markdown", "at": {"atMobiles": [], "isAtAll": node_at_all},
-                            "markdown": {"title": cluster_name, "text": content}}
-                    if not send_ding(data, robot):
-                        logging.error("Node发送钉钉告警失败！")
+                last_time = (condition.last_heartbeat_time + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+                if (condition.type == 'Ready' and condition.status != 'True') or (condition.type != 'Ready' and condition.status != 'False'):
+                    content = NODE_MSG_TEMPLATE % ('【' + self.cluster_name + '】 Node异常监控', 'Node', 'Error', event['object'].metadata.name, condition.message, condition.reason, last_time)
+                    # Node异常告警不做消息收敛，需要及时处理
+                    data = {"msgtype": "markdown", "at": {"atMobiles": [], "isAtAll": self.node_at_all}, "markdown": {"title": self.cluster_name, "text": content}}
+                    post_data = json.dumps(data).encode(encoding='UTF8')
+                    requests.post(self.ding_url, headers={'Content-Type': 'application/json; charset=utf-8'}, data=post_data)
+
+
+class EsHandler:
+    def __init__(self, cluster_name, es_host):
+        self.cluster_name = cluster_name
+        try:
+            self.es = Elasticsearch(hosts=es_host)
+            self.es.index
         except:
-            logging.error("解析Node数据异常！")
+            self.es = False
+            logging.error("初始化es失败")
+
+    def __call__(self, event):
+        if not self.es:
+            return False
+        if event['object'].kind == 'Event':
+            doc = {}
+            doc['Message'] = event['object'].message
+            doc['Reason'] = event['object'].reason
+            doc['Name'] = event['object'].metadata.name
+            doc['Namespace'] = event['object'].metadata.namespace
+            doc['Type'] = 'Event'
+            doc['Level'] = event['object'].type
+            doc['ClusterName'] = self.cluster_name
+            doc['Timestamp'] = (event['object'].last_timestamp + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+            doc['timestamp'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000+0800")
+            try:
+                self.es.index(index=self.cluster_name.lower(), doc_type="text", body=doc)
+            except ConnectionTimeout:
+                logging.error("连接ES超时！")
+            except:
+                logging.error("Event事件保存ES失败！")
+        else:
+            conditions = event['object'].status.conditions
+            for condition in conditions:
+                if (condition.type == 'Ready' and condition.status != 'True') or (condition.type != 'Ready' and condition.status != 'False'):
+                    doc = {}
+                    doc['Message'] = condition.message
+                    doc['Reason'] = condition.reason
+                    doc['Name'] = event['object'].metadata.name
+                    doc['Type'] = 'Node'
+                    doc['Level'] = 'Error'
+                    doc['ClusterName'] = self.cluster_name
+                    doc['Timestamp'] = (event['object'].last_timestamp + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+                    doc['timestamp'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.000+0800")
+                    try:
+                        self.es.index(index=self.cluster_name.lower(), doc_type="text", body=doc)
+                    except ConnectionTimeout:
+                        logging.error("连接ES超时！")
+                    except:
+                        logging.error("Node事件保存ES失败！")
 
 
 def main():
-    logging.info("集群监控机器人开始工作...")
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--in-cluster', action='store_true', help='configure with in cluster kubeconfig')
+    arg_parser.add_argument('--stdout', action='store_true', help='print events to stdout')
+    arg_parser.add_argument('--log-level', default='WARNING')
+    arg_parser.add_argument('--level', default='Warning')
+    arg_parser.add_argument('--ding-robot', help='send events to ding ding robot')
+    arg_parser.add_argument('--cluster-name', default='K8S集群')
+    arg_parser.add_argument('--times', default=10)
+    arg_parser.add_argument('--interval', default=180)
+    arg_parser.add_argument('--event-at-all', default=False)
+    arg_parser.add_argument('--node-at-all', default=True)
+    arg_parser.add_argument('--es-host', default='')
 
-    if "CLUSTER_NAME" in os.environ:
-        cluster_name = os.environ["CLUSTER_NAME"]
+    logging.error("集群监控机器人开始工作...")
+
+    args = arg_parser.parse_args()
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=args.log_level)
+
+    if args.in_cluster:
+        kubernetes.config.load_incluster_config()
     else:
-        logging.info("Please Set CLUSTER NAME....")
-        sys.exit(1)
+        kubernetes.config.load_kube_config()
 
-    if "DING_ROBOT" in os.environ:
-        robot = os.environ["DING_ROBOT"]
-    else:
-        logging.info("Please Set DING ROBOT....")
-        sys.exit(1)
+    signal.signal(signal.SIGINT, shutdown)
+    signal.signal(signal.SIGTERM, shutdown)
 
-    if "LEVEL" in os.environ:
-        level = os.environ["LEVEL"]
-    else:
-        level = 'Warning'
+    q = queue.Queue()
 
-    if "API_HOST" in os.environ:
-        host = os.environ["API_HOST"]
-    else:
-        host = "https://kubernetes.default.svc.cluster.local"
+    event = EventWatcherThread(q)
+    event.start()
 
-    if "TIMES" in os.environ:
-        times = int(os.environ["TIMES"])
-    else:
-        times = 15
-
-    if "INTERVAL" in os.environ:
-        interval = int(os.environ["INTERVAL"])
-    else:
-        interval = 300
-
-    if "POD_AT_ALL" in os.environ:
-        if os.environ["POD_AT_ALL"].lower() == 'true':
-            pod_at_all = True
-        else:
-            pod_at_all = False
-    else:
-        pod_at_all = False
-
-    if "NODE_AT_ALL" in os.environ:
-        if os.environ["NODE_AT_ALL"].lower() == 'true':
-            node_at_all = True
-        else:
-            node_at_all = False
-    else:
-        node_at_all = True
-        
-    if "ES_HOST" in os.environ:
-        es_host = os.environ["ES_HOST"]
-    else:
-        es_host = ''
-
-    config.load_incluster_config()
-    configuration = client.Configuration()
-    # configuration.host = host
-    configuration.verify_ssl = False
-    v1 = client.CoreV1Api(client.ApiClient(configuration))
-
-    pod = threading.Thread(target=pod_event, args=(v1, level, cluster_name, robot, pod_at_all, times, interval, es_host))
-    pod.setDaemon(True)
-    pod.start()
-
-    node = threading.Thread(target=node_event, args=(v1, 'Error', cluster_name, robot, node_at_all, es_host))
-    node.setDaemon(True)
+    node = NodeWatcherThread(q)
     node.start()
 
-    node.join()
+    handlers = []
 
+    if args.stdout:
+        handlers.append(print_handler)
+    if args.ding_robot:
+        handlers.append(RobotHandler(args.cluster_name, args.level, args.ding_robot, args.event_at_all, args.node_at_all))
+    if args.es_host:
+        handlers.append(EsHandler(args.cluster_name, args.es_host))
+
+    while True:
+        event = q.get()
+
+        if isinstance(event, Exception):
+            event.thread.join()
+            sys.exit(1)
+
+        for handle in handlers:
+            handle(event)
+
+def shutdown(signum, frame):
+    logging.error("Shutting down")
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
